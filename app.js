d8232ed851b81c09dfac94f70484deb0cc684f14
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-10-20 05:38:03 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Quiz data (array of objects) 
+var quizQue = [
+  {
+    question : "",
+    opt1 : "",
+    opt2 : "",
+    opt3 : ""
+  },
+  {
+    question : "",
+    opt1 : "",
+    opt2 : "",
+    opt3 : ""
+  },
+  {
+    question : "",
+    opt1 : "",
+    opt2 : "",
+    opt3 : ""
+  },
+  {
+    question : "",
+    opt1 : "",
+    opt2 : "",
+    opt3 : ""
+  },
+]
