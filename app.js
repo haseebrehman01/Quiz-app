a5e2c7d90cd77c6ecb2865e99fa8cdd29cb47908
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-10-25 06:15:20 +0500

    update

--- app.js
+++ app.js
@@ -3,35 +3,84 @@ var quizQue = [
   {
     question : "Q1 : What is HTML?",
     opt1 : "a) HTML describes the structure of a webpage",
-    opt2 : "b) HTML is the standard markup language mainly used to create web pages",
-    opt3 : "c) HTML consists of a set of elements that helps the browser how to view the content",
+    opt2 : "b) HTML is the standard markup language",
+    opt3 : "c) HTML consists of a set of elements ",
     opt4 : "d) All of the mentioned" ,
     correctOpt : "d) All of the mentioned"
   },
-  // {
-  //   question : "Who is the father of HTML?",
-  //   opt1 : "a) Rasmus Lerdorf",
-  //   opt2 : "b) Tim Berners-Lee",
-  //   opt3 : "c) Brendan Eich",
-  //   opt4 : "d) Sergey Brin" ,
-  //   correctOpt : "b) Tim Berners-Lee"
-  // },
-  // {
-  //   question : "HTML stands for __________",
-  //   opt1 : "a) HyperText Markup Language",
-  //   opt2 : "b) HyperText Machine Language",
-  //   opt3 : "c) HyperText Marking Language",
-  //   opt4 : "d) HighText Marking Language" ,
-  //   correctOpt : "a) HyperText Markup Language"
-  // },
-  // {
-  //   question : "What is the correct syntax of doctype in HTML5?",
-  //   opt1 : "a) </doctype html>",
-  //   opt2 : "b) <doctype html>",
-  //   opt3 : "c) <doctype html!>",
-  //   opt4 : "d) <!doctype html>" ,
-  //   correctOpt : "d) <!doctype html>"
-  // }
+  {
+    question : "Q2 : Who is the father of HTML?",
+    opt1 : "a) Rasmus Lerdorf",
+    opt2 : "b) Tim Berners-Lee",
+    opt3 : "c) Brendan Eich",
+    opt4 : "d) Sergey Brin" ,
+    correctOpt : "b) Tim Berners-Lee"
+  },
+  {
+    question : "Q3 : HTML stands for __________",
+    opt1 : "a) HyperText Markup Language",
+    opt2 : "b) HyperText Machine Language",
+    opt3 : "c) HyperText Marking Language",
+    opt4 : "d) HighText Marking Language" ,
+    correctOpt : "a) HyperText Markup Language"
+  },
+  {
+    question : "Q4 : What is the correct syntax of doctype in HTML5?",
+    opt1 : "a) </doctype html>",
+    opt2 : "b) <doctype html>",
+    opt3 : "c) <doctype html!>",
+    opt4 : "d) <!doctype html>" ,
+    correctOpt : "d) <!doctype html>"
+  },
+  {
+    question : "Q5 : What is DOM in HTML?",
+    opt1 : "a) Language dependent application programming",
+    opt2 : "b) Hierarchy of objects in ASP.NET",
+    opt3 : "c) Application programming interface",
+    opt4 : "d) Convention for representing and interacting with objects in html documents" ,
+    correctOpt : "d) Convention for representing and interacting with objects in html documents"
+  },
+  {
+    question : "Q6 : In which part of the HTML metadata is contained?",
+    opt1 : "a) head tag",
+    opt2 : "b) title tag",
+    opt3 : "c) html tag",
+    opt4 : "d) body tag" ,
+    correctOpt : "a) head tag"
+  },
+  {
+    question : "Q7 : Which element is used to get highlighted text in HTML5?",
+    opt1 : "a) <u>",
+    opt2 : "b) <mark>",
+    opt3 : "c) <highlight>",
+    opt4 : "d) <b>" ,
+    correctOpt : "b) <mark>"
+  },
+  {
+    question : "Q8 : Which of the following is not a HTML5 tag?",
+    opt1 : "a) <track>",
+    opt2 : "b) <video>",
+    opt3 : "c) <slider>",
+    opt4 : "d) <source>" ,
+    correctOpt : "c) <slider>"
+  },
+  {
+    question : "Q9 : How do we write comments in HTML?",
+    opt1 : "a) </…….>",
+    opt2 : "b) <!……>",
+    opt3 : "c) </……/>",
+    opt4 : "d) <…….!>" ,
+    correctOpt : "b) <!……>"
+  },
+  {
+    question : "Q10 : Which of the following elements in HTML5 defines video or movie content?",
+    opt1 : "a) <video>",
+    opt2 : "b) <movie>",
+    opt3 : "c) <audio>",
+    opt4 : "d) <media>" ,
+    correctOpt : "a) <video>"
+  }
+  
 ]
 
 // getting elements
@@ -130,6 +179,7 @@ roptions[i].checked = false  //radio off work (auto)
 
 
   }
+
 //  resetQuestionTimer();
   // sabsa last mai chalega
  
@@ -137,22 +187,40 @@ roptions[i].checked = false  //radio off work (auto)
 
         var resultt =  (number / quizQue.length) *100 + "%"  
 
- 
-inner_div.innerHTML =`<div class="last-work"><div class="s">
-<h1 class="last-h2">Congratulation you complete your html quiz <i class="fa-solid fa-face-grin-hearts" style="color: #ffdd00;"></i></h1>
-<h2 class="last-h3">you got ${number} / ${b}</h2>
-<h3 class="last-h4">Your percentage is ${resultt}</h3></div>
+ if (number > 5){  
+  inner_div.style.marginTop = "110px";
+  inner_div.innerHTML =`<div class="last-work"><div class="s">
+<h1 class="last-h2"><span class="pass">Congratulations you are passed <i class="fa-solid fa-face-grin-hearts" style="color: #ffdd00;"></i></h1>
+<h2 class="last-h3">you got <span class="pass">${number}</span> / ${b}</h2>
+<h3 class="last-h4">Your percentage is <span class="pass">${resultt}</span></h3></div>
 <div class="last-btns"> 
-    <a href=""><button class="last-btn">Home</button></a>
+    <a href="home.html"><button class="last-btn">Home</button></a>
     <a href=""><button class="last-btn a">Play Again</button></a>
-    <a href=""><button class="last-btn">Logout</button></a>
+    <a href="index.html"><button class="last-btn">Logout</button></a>
 </div></div>`
+ }
+ else{
+  inner_div.style.marginTop = "110px";
+  inner_div.innerHTML =`
+  <div class="last-work"><div class="s">
+<h1 class="last-h2"> <span class="failed">Sorry! you are failed</span> <i class="fa-regular fa-face-frown-open" style="color: #ffdd00;"></i>
+  </h1>
+<h2 class="last-h3">you got <span class="failed">${number}</span> / ${b}</h2>
+<h3 class="last-h4">Your percentage is <span class="failed">${resultt}</span></h3></div>
+<div class="last-btns"> 
+    <a href="home.html"><button class="last-btn">Home</button></a>
+    <a href=""><button class="last-btn a">Play Again</button></a>
+    <a href="index.html"><button class="last-btn">Logout</button></a>
+</div></div>`
+ }
+
     }
     else{
       //  first work of func func call hota hi yeh chalega
       // jab tak q khatam nahi honga chalta rahega
 
       topresult.innerHTML = `${number} / ${b}`
+      var num = quizQue[index].question
       questions.innerHTML = quizQue[index].question
 option1.innerText =  quizQue[index].opt1
 option2.innerText =  quizQue[index].opt2
